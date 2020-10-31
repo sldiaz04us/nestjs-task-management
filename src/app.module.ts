27ddabe79251bcf4abc00c2f72912d3bb612d653
Sergio Lopez Diaz <sldiaz04us@gmail.com>
2020-10-31 00:32:24 -0500

    TypeORM configuration added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TasksModule } from './tasks/tasks.module';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot(typeOrmConfig)
+  ],
   controllers: [],
   providers: [],
 })
