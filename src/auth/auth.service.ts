39bf1871a7a65b15848e56b77f4226b71d8d89ef
Sergio Lopez Diaz <sldiaz04us@gmail.com>
2020-11-05 16:58:52 -0600

    Added all necessary dependencies for JWT functionality. Sign in feature with JWT implemented.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,31 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository: UserRepository
+        private userRepository: UserRepository,
+        private jwtService: JwtService
     ) { }
 
     async signup(authCredentialDto: AuthCredentialDto) {
         return this.userRepository.signup(authCredentialDto);
     }
 
-    async signin(authCredentialDto: AuthCredentialDto) {
+    async signin(authCredentialDto: AuthCredentialDto): Promise<{ accessToken: string }> {
         const username = await this.userRepository.validateUserPassword(authCredentialDto);
         if (!username) {
             throw new UnauthorizedException('Invalid credentials');
         }
+
+        const payload: JwtPayload = { username };
+        const accessToken = this.jwtService.sign(payload);
+        return { accessToken };
     }
 }
